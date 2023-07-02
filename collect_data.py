372697f0138af7ddc1e8edf918b66a5e25fbff00
= <=>
2023-07-02 19:46:39 +0300

    some changes on collecting data

--- collect_data.py
+++ collect_data.py
@@ -4,41 +4,51 @@ import pyautogui
 import keyboard
 import time
 import cv2
+import pathlib
 
-X = 225
-Y = 150
-W = 625
-H = 200
+# coordinates of region of interest
+X = 320
+Y = 210
+W = 100
+H = 100
 
-FPS = 10
-time_delta = 1./FPS # 10 frames per second
+FPS = 100
+wait_time = 1./FPS # 100 frames per second
 
-target_data = [] # if user press space button add 1 else 0 
+pathlib.Path('./source/UP').mkdir(parents=True, exist_ok=True)
+pathlib.Path('./source/DOWN').mkdir(parents=True, exist_ok=True)
+pathlib.Path('./source/GROUND').mkdir(parents=True, exist_ok=True)
+#erase_old_images()
+
+# create dataset directories if not exists
+
+status = 'GROUND' 
 print("[INFO] Gathering data from your play for dataset...")
 
-i = 1
+i = 0
 
 try:
     while True:
-        if keyboard.is_pressed('space'):
-            target_data.append(1)
+        if keyboard.is_pressed('up'):
+            status = 'UP'
+        elif keyboard.is_pressed('down'):
+            status = 'DOWN'
         else:
-            target_data.append(0)
+            status = 'GROUND'
 
         screenshot = pyautogui.screenshot() # take screenshot of whole screen
         
         img = np.array(screenshot) # convert to numpy array
         img = img[Y:Y+H, X:X+W] # coordinates of roi
         
-        image = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) # convert RGB to BGR for opencv
-        image_path = "images/image" + str(i) + ".jpg" 
+        image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # convert RGB to BGR for opencv
+        image_path = f'source/{status}/image-{i:05d}.jpg'
+ 
         cv2.imwrite(image_path, image)
 
         i+=1
-        time.sleep(time_delta) 
+        time.sleep(wait_time) 
 
 except KeyboardInterrupt:
-    pass
+    print('Keyboard interrupted.')
 
-df = pd.DataFrame(target_data)
-df.to_csv("data/target_data.csv", sep=",")
